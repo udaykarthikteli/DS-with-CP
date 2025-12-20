23443160fce5651d7f3671899eb7d3ba13ff60e2
udaykarthikteli <udaykarthikteli745@gmail.com>
2025-12-20 16:05:54 +0530

    Update task1.java

--- Module1_Array/task1.java
+++ Module1_Array/task1.java
@@ -1,8 +1,7 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
 class Main {
     public static void main(String[] args) {
+        
+        // static D& I
         int[]n={10,20,30,40};
         System.out.println(n[0]);
         System.out.println(n[1]);
@@ -13,7 +12,7 @@ class Main {
         for(int i=0; i<n.length; i++){
             System.out.println("at index "+ i + " is " + n[i]);
         }
-        
+        // static D& I we are using new key word
         int[] n1= new int[] {1,2,3,4};
         
         for(int i=0; i<n1.length; i++){
@@ -24,12 +23,18 @@ class Main {
         for(int i=0; i<names.length; i++){
             System.out.println("at index "+ i + " is " + names[i]);
         }
-         int[] darr= new int[10]; 
-
-        darr[0]=1;
-        darr[9]=8;
-       for (int i = 0; i < darr.length; i++) {
-        System.out.println(darr[i]);
+        
+        // Dynamic Allocation
+        int[] n3= new int[10];
+        n3[0]=200;
+        n3[9]=2000;
+        
+        for(int i=0; i<n3.length; i++){
+            System.out.println("at index "+ i + " is " + n3[i]);
+        }
+        
+        
+        
+        
     }
-}
 }
