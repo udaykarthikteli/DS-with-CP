7499c708aee93a2e74abc6c21243aa60a9be8ba8
udaykarthikteli <udaykarthikteli745@gmail.com>
2026-01-09 14:43:40 +0530

    Create NodeConstructor.java

---
+++ Module1_Array/NodeConstructor.java
@@ -0,0 +1,26 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class Node{
+    int data;
+    Node next;
+    
+    Node(int a){
+        data = a;
+        next=null;
+    }
+    
+    public void getData(){
+        System.out.println(data);
+    }
+    
+    public void getNext(){
+        System.out.println(next);
+        
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Node a = new Node(10);
+       System.out.println(a);
+    }
+}
